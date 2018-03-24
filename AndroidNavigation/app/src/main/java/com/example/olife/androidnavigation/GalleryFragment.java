5cd9bf37af5540982381f3e6191fb849c2765718
khayrulislam <bsse0822@iit.du.ac.bd>
2018-03-24 23:07:27 +0600

    android navigation drawer code for multiple window

---
+++ AndroidNavigation/app/src/main/java/com/example/olife/androidnavigation/GalleryFragment.java
@@ -0,0 +1,24 @@
+package com.example.olife.androidnavigation;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GalleryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_gallery_fragment,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Gallery");
+    }
+}
---
+++ AndroidNavigation/app/src/main/java/com/example/olife/androidnavigation/SendFragment.java
@@ -0,0 +1,26 @@
+package com.example.olife.androidnavigation;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+public class SendFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_send_fragment,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Send");
+    }
+}
